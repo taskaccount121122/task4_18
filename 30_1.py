9ce4e145d56a2624c57205ba581151ff9b704446
Rena Huseynli <RenaHuseynli12412@gmail.com>
2022-12-08 19:20:46 +0400

    new program code

--- 30_1.py
+++ 30_1.py
@@ -8,6 +8,8 @@ data["server"]["port"] = 2024
 data["server"]["port2"] = 2025
 data["openInBrowser"] = True
 data["dist"]["fonts"] = "Montserrat"
+data["dist"]["fonts"] = "Pier Sans"
+data["version"] = "1.0.1"
  
 with open("my_config.json", "w") as file:
     json.dump(data, file)
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# task4_18
+# Task4.18
+## This repository is created to complete the task 4.18.
+## It contains the python file 30_1.py and json file config1.json
+## Last changes made: 08-12-2022
